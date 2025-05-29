572ddb4dffd33f87afcba2d39c7eea3cb6d56457
QaisAlsaid <qaisalsaid4@gmail.com>
2025-05-29 04:57:58 +0000

    fix

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -6,14 +6,14 @@
   ];
 
   # home-manager options go here
-  home.packages = with pkg; [
+  home.packages = with pkgs; [
     # pkgs.vscode - hydenix's vscode version
     # pkgs.userPkgs.vscode - your personal nixpkgs version
     brave
     zed-editor
     btop
     openvpn
-    wireguard
+    wireguard-tools
     protonvpn-gui
 
   ];
